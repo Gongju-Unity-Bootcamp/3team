4f16ae995a1fcc87bc6f1196dacac75e7b29d3be
Jinho Lee <bravearc69@gmail.com>
2024-03-13 14:21:34 +0900

    마커 UI추가

--- 3team/Assets/Scripts/Manager/DataManager.cs
+++ 3team/Assets/Scripts/Manager/DataManager.cs
@@ -4,14 +4,46 @@ using UnityEngine;
 using CsvHelper;
 using System.Linq;
 using System.IO;
+using System.Globalization;
+using System;
+using System.Diagnostics.CodeAnalysis;
 
 public class DataManager : MonoBehaviour
 {
 
-    Dictionary<MapID, MapData> Map = new Dictionary<MapID, MapData>();
-    //Dictionary<>
+    public Dictionary<MapID, MapData> Map = new Dictionary<MapID, MapData>();
+    public Dictionary<SoundID, SoundData> Sound = new Dictionary<SoundID, SoundData>();
+    private void Awake()
+    {
+        Init();
+    }
+
     public void Init()
     {
+#if UNITY_EDITOR
+        Map = ParseToDict<MapID, MapData>("Assets/Resources/Data/Item.csv", data => data.Id);
+        Sound = ParseToDict<SoundID, SoundData>("Assets/Resources/Data/Sound.csv", data => data.Id);
+#else
+        TextAsset mapCSV = Resources.Load<TextAsset>("Data/Map");
+        Item = ParseToDict<MapID, MapData>(MapCSV.text, data => data.Id);
 
+        TextAsset soundCSV = Resources.Load<TextAsset>("Data/Sound");
+        Sound = ParseToDict<SoundID, SoundData>(soundCSV.text, data => data.Id);
+#endif
+
+    }
+
+    private Dictionary<TKey, TItem> ParseToDict<TKey, TItem>([NotNull] string path, Func<TItem, TKey> KeySelector)
+    {
+        string fullPath = path;
+#if UNITY_EDITOR
+        using (var reader = new StreamReader(fullPath))
+#else
+        using (var reader = new StringReader(fullPath))
+#endif
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            return csv.GetRecords<TItem>().ToDictionary(KeySelector);
+        }
     }
 }
